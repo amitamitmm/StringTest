9ddbc273d89b6b4b7992992f11d7c92ca1df47f3
Amit Choudhary <amitamitmm@gmail.com>
2023-07-16 16:42:02 +0530

    Update main method

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is first application commit to github from intelliJ");
+        System.out.println("This is first application commit to github from intelliJ");
     }
 }
